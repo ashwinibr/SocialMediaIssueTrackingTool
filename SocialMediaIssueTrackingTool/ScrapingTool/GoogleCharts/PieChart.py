b1f300e40a710ce496c8419422eb92663413ea91
anandudagatti <anand.udagatti@ltts.com>
2019-04-09 17:15:58 +0530

    Updated Dashboard

--- SocialMediaIssueTrackingTool/ScrapingTool/GoogleCharts/PieChart.py
+++ SocialMediaIssueTrackingTool/ScrapingTool/GoogleCharts/PieChart.py
@@ -27,12 +27,12 @@ def Create_Pie_Chart():
 
   function drawPieChart() {
       var PieData = google.visualization.arrayToDataTable([
-        ["Category", "NrOfIssues"],
+        ["Category", "Issues"],
       $res
       ]);
 
       var options = {
-        title: "Number of Comments By Tags",
+        title: "Number of Issues By Category",
         curveType: "function",
         legend: { position: "right" }
       };
@@ -58,12 +58,12 @@ def Create_Pie_Chart():
 
 def Create_Column_Chart():
   conn = sqlite3.connect('db.sqlite3')
-  cursor = conn.execute("SELECT Date, sum(NrOfIssues) from Issues_Count_By_Keyword GROUP By Date ORDER BY sum(NrOfIssues) DESC")
+  cursor = conn.execute("SELECT Date, Category, sum(NrOfIssues) from Issues_Count_By_Keyword GROUP By Date, Category ORDER BY Date")
 
   results = []
 
   for row in cursor:
-      results.append({'Date': row[0], 'NrOfIssues': row[1]})
+      results.append({'Date': row[0], 'Category':row[1], 'NrOfIssues': row[2]})
 
   template = '''
   <html>
@@ -82,14 +82,15 @@ def Create_Column_Chart():
 
   function drawPieChart() {
       var ColData = google.visualization.arrayToDataTable([
-        ["Date", "NrOfIssues"],
+        ["Date", "Category", "Issues"],
       $res
       ]);
 
       var options = {
-        title: "Number of Comments By Tags",
+        title: "Number of Issues By Date and Category",
         curveType: "function",
         legend: { position: "right" }
+        isStacked: true,
       };
 
       var chart = new google.visualization.ColumnChart(document.getElementById('Col_chart'));
@@ -108,5 +109,5 @@ def Create_Column_Chart():
   </html>'''
 
   with open('ScrapingTool\\templates\\colchart.html', 'w') as html:
-      ColData = ','.join(['["{Date}", {NrOfIssues}]'.format(**r) for r in results])
+      ColData = ','.join(['["{Date}", {Category}, {NrOfIssues}]'.format(**r) for r in results])
       html.write(Template(template).substitute(res=ColData))
